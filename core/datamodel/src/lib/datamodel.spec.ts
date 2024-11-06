0499892da4841997489ddc1069bbfb38b9dd96dd
Thiago M <mail.thiago.moreira@gmail.com>
2024-11-06 02:12:29 -0300

    ambiente de desenvolvimento

---
+++ core/datamodel/src/lib/datamodel.spec.ts
@@ -0,0 +1,7 @@
+import { datamodel } from './datamodel';
+
+describe('datamodel', () => {
+  it('should work', () => {
+    expect(datamodel()).toEqual('datamodel');
+  });
+});
---
+++ core/datamodel/src/lib/datamodel.ts
@@ -0,0 +1,3 @@
+export function datamodel(): string {
+  return 'datamodel';
+}
